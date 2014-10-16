86096b0afd86052af8391d98ba7c3c4f243e0c55
Brandon95 <brandonboomhower@gmail.com>
2014-10-16 08:45:30 -0400

    Finished demo
    all three demo folders contain finished exercises

--- demo3/script.js
+++ demo3/script.js
@@ -1,12 +1,16 @@
 var username="Fred";
-document.onclick=
-function(evt) {
+//languages offer other useful events that onclick
+document.onmousemove=
+function(evt){
 		makeOneCircle(evt);
 }
 
 function makeOneCircle(evt){
-	evt = (evt || event)//makes all browzers understand
+	evt = (evt || event)//makes all browsers understand
 	//make a new circle
-	var newc = document.getElementsByTagName("div");
-	document.appendChild(newc);
+	var newc = document.createElement("div");
+	//set location of the new div to where the mouse is
+	newc.style.left = evt.clientX+"px";
+	newc.style.top = evt.clientY+"px";
+	document.body.appendChild(newc);
 }
